b2451bb823f2c463be5aa43613895b26fb0cbe24
ziwenlin <zw.lin1998@hotmail.com>
2023-04-04 15:56:58 +0200

    Created new class View to replace the older View

--- src/SerialPlotter/panels/configure.py
+++ src/SerialPlotter/panels/configure.py
@@ -37,6 +37,91 @@ class View(mvc.ViewOld):
         self.create_grouped_button('Buttons', 'Load', 'Restore settings')
 
 
+class ViewNew(mvc.ViewBase):
+    def __init__(self, master):
+        super().__init__(master)
+        expand_cnf = {'sticky': 'ns' + 'we'}
+        frame_cnf = {'padx': 5, 'pady': 5}
+
+        # widget containers
+        label = self.labels
+        entry = self.entries
+        button = self.buttons
+        spinbox = self.spinboxes
+
+        # Layout configuration
+        self.frame.grid_columnconfigure(0, uniform='a')
+        self.frame.grid_columnconfigure(1, uniform='a')
+        self.frame.grid_columnconfigure(2, uniform='a')
+        self.frame.grid_rowconfigure(99, weight=1)
+
+        # Frame for application settings
+        frame_application = tk.LabelFrame(self.frame, text='Application settings', cnf=frame_cnf)
+        frame_application.grid_configure(row=0, column=0, rowspan=2, columnspan=3, cnf=expand_cnf)
+        frame_application.columnconfigure(0, uniform='a', weight=1)
+        frame_application.columnconfigure(1, uniform='a', weight=1)
+        frame_application.columnconfigure(2, uniform='a', weight=1)
+
+        # Contents application settings frame
+        label['app_title'] = mvc.Label(frame_application, 'Application name')
+        label['app_title'].grid_configure(row=1, column=0)
+        entry['app_title'] = mvc.Entry(frame_application)
+        entry['app_title'].grid_configure(row=1, column=1, columnspan=2)
+        entry['app_title'].grid_configure(cnf=expand_cnf)
+
+        # Frame for graph view settings
+        frame_graph = tk.LabelFrame(self.frame, text='Graph view settings', cnf=frame_cnf)
+        frame_graph.grid_configure(row=2, column=0, rowspan=2, columnspan=3, cnf=expand_cnf)
+        frame_graph.columnconfigure(0, uniform='a', weight=1)
+        frame_graph.columnconfigure(1, uniform='a', weight=1)
+        frame_graph.columnconfigure(2, uniform='a', weight=1)
+
+        # Contents graph view settings
+        label['graph_x_axis'] = mvc.Label(frame_graph, 'Range x-axis')
+        label['graph_y_axis'] = mvc.Label(frame_graph, 'Range y-axis')
+        label['graph_min'] = mvc.Label(frame_graph, 'Minimum')
+        label['graph_max'] = mvc.Label(frame_graph, 'Maximum')
+        label['graph_min'].grid_configure(row=0, column=1)
+        label['graph_max'].grid_configure(row=0, column=2)
+        label['graph_x_axis'].grid_configure(row=1)
+        label['graph_y_axis'].grid_configure(row=2)
+
+        # Contents graph view settings
+        spinbox['graph_x_min'] = mvc.Spinbox(frame_graph, (-1000, 0, 10))
+        spinbox['graph_x_max'] = mvc.Spinbox(frame_graph, (10, 10000, 10))
+        spinbox['graph_y_min'] = mvc.Spinbox(frame_graph, (-1000, 0, 10))
+        spinbox['graph_y_max'] = mvc.Spinbox(frame_graph, (10, 10000, 10))
+        spinbox['graph_x_min'].grid_configure(row=1, column=1, cnf=expand_cnf)
+        spinbox['graph_x_max'].grid_configure(row=1, column=2, cnf=expand_cnf)
+        spinbox['graph_y_min'].grid_configure(row=2, column=1, cnf=expand_cnf)
+        spinbox['graph_y_max'].grid_configure(row=2, column=2, cnf=expand_cnf)
+
+        # Frame for graph data settings
+        frame_data = tk.LabelFrame(self.frame, text='Graph data settings', cnf=frame_cnf)
+        frame_data.grid_configure(row=4, column=0, rowspan=2, columnspan=3, cnf=expand_cnf)
+        frame_data.columnconfigure(0, uniform='a', weight=1)
+        frame_data.columnconfigure(1, uniform='a', weight=1)
+        frame_data.columnconfigure(2, uniform='a', weight=1)
+
+        # Contents of graph data settings frame
+        label['graph_data_size'] = mvc.Label(frame_data, 'Total of points')
+        label['graph_clear_size'] = mvc.Label(frame_data, 'Clear amount')
+        label['graph_data_size'].grid_configure(row=3)
+        label['graph_clear_size'].grid_configure(row=4)
+
+        # Contents of graph data settings frame
+        spinbox['graph_data_size'] = mvc.Spinbox(frame_data, (10, 10000, 10))
+        spinbox['graph_clear_size'] = mvc.Spinbox(frame_data, (10, 10000, 10))
+        spinbox['graph_data_size'].grid_configure(row=3, column=1, cnf=expand_cnf)
+        spinbox['graph_clear_size'].grid_configure(row=4, column=1, cnf=expand_cnf)
+
+        # Buttons for saving settings inside this view
+        button['save'] = mvc.Button(self.frame, 'Save settings')
+        button['restore'] = mvc.Button(self.frame, 'Restore settings')
+        button['save'].grid_configure(row=100, column=1, cnf=expand_cnf)
+        button['restore'].grid_configure(row=100, column=2, cnf=expand_cnf)
+
+
 class Controller(mvc.ControllerOld):
     def __init__(self, master: tk.Frame, interface: TaskInterface):
         self.interface = interface
